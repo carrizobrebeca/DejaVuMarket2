41e4f38f4fa60b4905aad1af7d621cc55e67c95d
carrizobrebeca <carrizob.rebeca@gmail.com>
2024-09-28 13:59:14 -0300

    redux

---
+++ src/store/productSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+  const response = await axios.get(`http://localhost:3001/products`);
+  return response.data;
+});
+
+// Slice de Redux
+const productSlice = createSlice({
+  name: 'products',
+  initialState: {
+    allProducts: [], 
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.allProducts = action.payload; // Asegúrate de que `allSuper` se actualice correctamente
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default productSlice.reducer;
